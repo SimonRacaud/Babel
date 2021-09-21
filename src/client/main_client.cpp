e896b053714981b82d525c30f60da8ee605da8f6
Arthur <arthur.jourdan@epitech.eu>
2021-09-21 22:04:23 +0200

    feat(CMakeLists.txt): add compilation for server and client sides

---
+++ src/client/main_client.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2021
+** main_client.cpp
+** File description:
+** Client side of Babel
+*/
+
+int main(const int, const char *[])
+{
+    return 0;
+}
---
+++ src/server/main_server.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2021
+** main_server.cpp
+** File description:
+** Server side of Babel
+*/
+
+int main(const int, const char *[])
+{
+    return 0;
+}
