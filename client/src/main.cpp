68c3f37d4ba7c30eb33f37b0f9a706935304ae81
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-09-23 17:01:55 +0200

    - Add end of exemple

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -31,15 +31,8 @@ static int streamerCallBack(const void *, void *output, unsigned long, const PaS
 {
     PortAudioCaps::AudioStreamer *tools = static_cast<PortAudioCaps::AudioStreamer *>(params);
     std::queue<Audio::rawFrameBuffer> &tab = tools->getSampleBuffer();
-
-    std::cout << "here1: " << tab.size() << std::endl;
     auto &member = tab.front();
-    std::memset(output, 0, Audio::FRAMES_PER_BUFFER * Audio::NUM_CHANNELS * sizeof(float));
 
-    /*float *testOutput = (float *) output;
-    for (size_t i = 0; i < Audio::FRAMES_PER_BUFFER * Audio::NUM_CHANNELS; i++) {
-        testOutput[i] += member.data[i];
-    }*/
     std::memcpy(output, member.data.data(), Audio::FRAMES_PER_BUFFER * Audio::NUM_CHANNELS * sizeof(float));
     tab.pop();
     return (tab.size()) ? paContinue : paComplete;
@@ -47,22 +40,24 @@ static int streamerCallBack(const void *, void *output, unsigned long, const PaS
 
 int main()
 {
-    // Audio::rawFrameBuffer Frame;
-    // Audio::compressFrameBuffer compressed;
+    /*
+    ** OPUS
+    */
+    Audio::compressFrameBuffer compressed;
 
-    // Frame.data = std::vector<float>(Audio::SAMPLE_RATE * Audio::NUM_CHANNELS * sizeof(float));
-    // std::fill(Frame.data.begin(), Frame.data.end(), 0);
+    compressed.data = std::vector<unsigned char>(Audio::FRAMES_PER_BUFFER * Audio::NUM_CHANNELS * sizeof(unsigned char));
+    std::fill(compressed.data.begin(), compressed.data.end(), 0);
+    compressed.encodedBit = 0;
 
-    // compressed.data = std::vector<unsigned char>(Audio::SAMPLE_RATE * Audio::NUM_CHANNELS * sizeof(unsigned char));
-    // std::fill(compressed.data.begin(), compressed.data.end(), 0);
-    // compressed.encodedBit = 0;
-
-    // OpusCaps::opusDecoder decode;
-    // OpusCaps::opusEncoder encode;
+    OpusCaps::opusDecoder decode;
+    OpusCaps::opusEncoder encode;
 
     // Frame = decode.extract(compressed);
     // compressed = encode.compress(Frame);
 
+    /*
+    ** PortAudio
+    */
     std::unique_ptr<PortAudioCaps::AudioRecorder> record = std::make_unique<PortAudioCaps::AudioRecorder>();
     record->setCallBack(recordCallBack);
     record->startStreaming();
@@ -72,15 +67,24 @@ int main()
     std::queue<Audio::rawFrameBuffer> buffer = record->getSampleBuffer();
     record.reset();
 
+    /*
+    ** COMPRESSION START
+    */
+    std::queue<Audio::compressFrameBuffer> listCompressed;
+    while (buffer.size()) {
+        listCompressed.push(encode.compress(buffer.front()));
+        buffer.pop();
+    }
+    while (listCompressed.size()) {
+        buffer.push(decode.extract(listCompressed.front()));
+        listCompressed.pop();
+    }
+    /*
+    ** COMPRESSION START
+    */
+
     std::unique_ptr<PortAudioCaps::AudioStreamer> streamer = std::make_unique<PortAudioCaps::AudioStreamer>();
 
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << "ICICICICICICICICICI: " << buffer.size() << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
     while (buffer.size()) {
         streamer->getSampleBuffer().push(buffer.front());
         buffer.pop();
