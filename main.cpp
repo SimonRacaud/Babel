11b8fc575bb3db9fb6d20ebd30cc15b388929cb0
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-09-23 11:18:45 +0200

    - Fix include path

--- main.cpp
+++ main.cpp
@@ -5,25 +5,33 @@
 ** opusDecoder.hpp
 */
 
+#include <memory>
+#include "AudioStreamer.hpp"
+#include "AudioRecorder.hpp"
 #include "opusDecoder.hpp"
 #include "opusEncoder.hpp"
 
 int main()
 {
-    Audio::rawFrameBuffer Frame;
-    Audio::compressFrameBuffer compressed;
+    // Audio::rawFrameBuffer Frame;
+    // Audio::compressFrameBuffer compressed;
 
-    Frame.data = std::vector<float>(Audio::SAMPLE_RATE * Audio::NUM_CHANNELS * sizeof(float));
-    std::fill(Frame.data.begin(), Frame.data.end(), 0);
+    // Frame.data = std::vector<float>(Audio::SAMPLE_RATE * Audio::NUM_CHANNELS * sizeof(float));
+    // std::fill(Frame.data.begin(), Frame.data.end(), 0);
 
-    compressed.data = std::vector<unsigned char>(Audio::SAMPLE_RATE * Audio::NUM_CHANNELS * sizeof(unsigned char));
-    std::fill(compressed.data.begin(), compressed.data.end(), 0);
-    compressed.encodedBit = 0;
+    // compressed.data = std::vector<unsigned char>(Audio::SAMPLE_RATE * Audio::NUM_CHANNELS * sizeof(unsigned char));
+    // std::fill(compressed.data.begin(), compressed.data.end(), 0);
+    // compressed.encodedBit = 0;
 
-    OpusCaps::opusDecoder decode;
-    OpusCaps::opusEncoder encode;
+    // OpusCaps::opusDecoder decode;
+    // OpusCaps::opusEncoder encode;
 
-    Frame = decode.extract(compressed);
-    compressed = encode.compress(Frame);
+    // Frame = decode.extract(compressed);
+    // compressed = encode.compress(Frame);
+
+    std::unique_ptr<PortAudioCaps::AudioRecorder> record = std::make_unique<PortAudioCaps::AudioRecorder>();
+    record->startStreaming();
+    record->endStreaming();
+    record.reset();
     return 0;
 }
