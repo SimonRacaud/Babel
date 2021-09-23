b4483d738aef6720ee236e2bcec5b9a9832cbd8d
simon.racaud <simon.racaud@epitech.eu>
2021-09-23 15:29:09 +0200

    chore(gui): option menu window

---
+++ client/src/gui/Options/Options.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT , 2021
+** Options
+** File description:
+** simon
+*/
+
+#include "Options.hpp"
+
+using namespace GUI;
+
+Options::Options()
+{
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
+    //mainLayout->addWidget(button);
+}
