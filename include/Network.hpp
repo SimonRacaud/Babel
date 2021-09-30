b47f779279c5d8f58191451d9d971d76f6d3d003
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-09-30 10:53:27 +0200

    - Add constructor to tcp tram

--- include/Network.hpp
+++ include/Network.hpp
@@ -8,16 +8,33 @@
 #ifndef NETWORK_HPP
 #define NETWORK_HPP
 
+#include <ctime>
+#include <cstring>
+
 namespace Network
 {
     const int BUFFER_SIZE = 1024;
+    const unsigned int MAGIC_NUMBER = 0x42;
 
     typedef struct UDPTram_s
     {
         unsigned int magicNumber;   // 4
         size_t timestamp;           // 8
-        char size[1008];            // 1024 - (4 * 2 + 8)
+        char data[1008];            // 1024 - (4 * 2 + 8)
         unsigned int dataSize;      // 4
+        UDPTram_s()
+        {
+            std::memset(this, 0, sizeof(UDPTram_s));
+            this->magicNumber = Network::MAGIC_NUMBER;
+            this->timestamp = std::time(0);
+        }
+        UDPTram_s(void *src)
+        {
+            std::memset(this, 0, sizeof(UDPTram_s));
+            this->magicNumber = Network::MAGIC_NUMBER;
+            this->timestamp = std::time(0);
+            std::memmove(this->data, src, sizeof(this->data));
+        }
     } UDPTram_t;
 }
 
