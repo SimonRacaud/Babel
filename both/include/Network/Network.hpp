577033c24ba2b115412a23382d73b245bd85ca57
Arthur <arthur.jourdan@epitech.eu>
2021-10-08 22:25:47 +0200

    test(both/include/Network/Network.hpp): change the ip, for sullivan's

--- both/include/Network/Network.hpp
+++ both/include/Network/Network.hpp
@@ -15,7 +15,7 @@
 namespace Network
 {
     const std::string IP_USER = "0.0.0.0";
-    const std::string IP_SERVER = "192.168.124.1";
+    const std::string IP_SERVER = "10.19.249.8";
     const int PORT_MAIN_SERVER = 8081;
     const int PORT_CALL_SERVER = 8082;
     const int PORT_UDP_RECEIVE = 8088;
