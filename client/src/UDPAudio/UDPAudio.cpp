3ab1508144d905afbf2d90c5bcaf9f48ef8b7238
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-10-16 21:05:35 +0200

    -

--- client/src/UDPAudio/UDPAudio.cpp
+++ client/src/UDPAudio/UDPAudio.cpp
@@ -129,7 +129,7 @@ void UDPAudio::receivingData()
 bool UDPAudio::correctPacket(size_t &lastTimestamp, const Network::UDPTram_t &tram)
 {
     bool magicNumber = tram.magicNumber == Network::MAGIC_NUMBER;
-    bool timestamp = lastTimestamp <= tram.timestamp || !Audio::DEFAULT_CHECKED_TIMESTAMP;
+    bool timestamp = true;//lastTimestamp <= tram.timestamp || !Audio::DEFAULT_CHECKED_TIMESTAMP;
 
     timestamp = tram.timestamp;
     return magicNumber && timestamp;
